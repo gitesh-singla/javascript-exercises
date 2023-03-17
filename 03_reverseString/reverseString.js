65c0fb89ab4f1596df55052d539e2d04422c1e71
gitesh <124420820+gitesh-singla@users.noreply.github.com>
2023-03-17 19:55:07 +0530

    Ex 1,2,3,5 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const num = string.length;
+    let array = [];
+    for(let i = num ; i>0 ; i--){
+        array[num - i] = string.charAt(i-1);
+    }
 
+    return array.join("");
 };
 
 // Do not edit below this line
